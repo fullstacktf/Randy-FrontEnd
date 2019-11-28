f229b0505217f059c650ffe1681a0618ace70d35
Rubén Zagon <ruben.zagon@gmail.com>
2019-11-28 12:00:09 +0000

    Prueba de despliegue

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,10 @@ echo "Importing  SSH Key."
 echo "REMOTE_USER = ${REMOTE_USER}"
 echo "REMOTE_HOST = ${REMOTE_HOST}"
 
-openssl aes-256-cbc -K $encrypted_af8de67677e8_key -iv $encrypted_af8de67677e8_iv -in travis-deploy-do-fron.enc -out keys/travis-deploy-do-fron -d
+openssl aes-256-cbc -K $encrypted_af8de67677e8_key -iv $encrypted_af8de67677e8_iv -in travis-deploy-do-fron.enc -out travis-deploy-do-fron -d
 
 eval "$(ssh-agent -s)"
 
-chmod 600 keys/travis-deploy-do-fron
+chmod 600 travis-deploy-do-fron
 
-ssh-add keys/travis-deploy-do-fron
+ssh-add travis-deploy-do-fron
