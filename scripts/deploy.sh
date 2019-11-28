69a76a7526c8d8584a46a4e40d290add5831d3a6
Rubén Zagon <ruben.zagon@gmail.com>
2019-11-28 12:03:45 +0000

    Prueba de despliegue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ echo "Deploying on digitalocean"
 
 COMMAND="cd ${REMOTE_PATH} && git pull"
 
-ssh -o StrictHostKeyChecking=no -i keys/travis-deploy-do-fron -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
+ssh -o StrictHostKeyChecking=no -i travis-deploy-do-fron -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
