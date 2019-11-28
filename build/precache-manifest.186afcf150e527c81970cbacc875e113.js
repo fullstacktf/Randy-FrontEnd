0de357a3cda4189f5c93e2a465675d21941ec450
Rubén Zagon <ruben.zagon@gmail.com>
2019-11-28 13:00:09 +0000

    pruebas de solo dejar build en esta rama

---
+++ build/precache-manifest.186afcf150e527c81970cbacc875e113.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8680d93b7201eb94eabcac2f0c9ea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46025f7dc3344cb123be",
+    "url": "/static/css/main.e2df56ff.chunk.css"
+  },
+  {
+    "revision": "563e887adf2893c6dadf",
+    "url": "/static/js/2.265bd4ef.chunk.js"
+  },
+  {
+    "revision": "46025f7dc3344cb123be",
+    "url": "/static/js/main.6c58f33f.chunk.js"
+  },
+  {
+    "revision": "4229b494eba961aa628b",
+    "url": "/static/js/runtime-main.aa1ff8ab.js"
+  },
+  {
+    "revision": "1566b90da1c930366e0dc11c03973a70",
+    "url": "/static/media/icon-bill.1566b90d.svg"
+  },
+  {
+    "revision": "9d27d040e431f9d423220c118b75ba87",
+    "url": "/static/media/icon-calendar.9d27d040.svg"
+  },
+  {
+    "revision": "d539ffcd8307616b020208d2d474d0ef",
+    "url": "/static/media/icon-dashboard.d539ffcd.svg"
+  },
+  {
+    "revision": "b99d7c86cd255358dd4fd7780996677a",
+    "url": "/static/media/icon-settings.b99d7c86.svg"
+  },
+  {
+    "revision": "1a1857e5ebd33ad0e9f3bdc132ae9105",
+    "url": "/static/media/icon-tasklist.1a1857e5.svg"
+  }
+]);
