3c4634cec2d9cd9395f33d08404faeb6f11ab54e
Rubén Zagon <ruben.zagon@gmail.com>
2019-11-28 12:36:48 +0000

    Prueba de despligue de solo la carpeta build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,8 @@ echo "Deploying on digitalocean"
 
 COMMAND="cd ${REMOTE_PATH} && git pull"
 
+npm run build
+
+git subtree push --prefix build origin production
+
 ssh -o StrictHostKeyChecking=no -i travis-deploy-do-fron -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
