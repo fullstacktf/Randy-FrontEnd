bf3c7f0adffdff921d06b983274d52cdfa153af9
raulanatol <raul@natol.es>
2019-12-02 20:47:44 +0000

    Update

--- src/components/navigator/navigator.tsx
+++ src/components/navigator/navigator.tsx
@@ -1,29 +1,35 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./navigator.css";
 import { Icon } from "./icon";
+import { useState } from "react";
 
-interface NavigatorProps {}
+interface NavigatorProps {
+}
+
+const NavIcon = ({ to, name }) => {
+  const [isActive, setIsActive] = useState(false);
+  return <NavLink to={to} isActive={(match, location) => {
+    if (!match) {
+      setIsActive(false);
+      return false;
+    }
+    setIsActive(true);
+    return true;
+  }}>
+    <Icon active={isActive} name={name}/></NavLink>;
+};
+
 
 class Navigator extends React.Component {
   render() {
     return (
       <div className="container">
-        <Link to="/dashboard">
-          <Icon name="dashboard" state="no-selected"></Icon>
-        </Link>
-        <Link to="/calendar">
-          <Icon name="calendar" state="no-selected"></Icon>
-        </Link>
-        <Link to="/tasklist">
-          <Icon name="tasklist" state="no-selected"></Icon>
-        </Link>
-        <Link to="/bill">
-          <Icon name="bill" state="no-selected"></Icon>
-        </Link>
-        <Link to="/settings">
-          <Icon name="settings" state="no-selected"></Icon>
-        </Link>
+        <NavIcon to="/dashboard" name="dashboard"/>
+        <NavIcon to="/calendar" name="calendar"/>
+        <NavIcon to="/tasklist" name="tasklist"/>
+        <NavIcon to="/bill" name="bill"/>
+        <NavIcon to="/settings" name="settings"/>
       </div>
     );
   }
