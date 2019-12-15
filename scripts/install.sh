4f6532a09a72e38c33700f59dc306d4a7c317361
Rubén Zagon <ruben.zagon@gmail.com>
2019-12-15 10:05:53 +0000

    Continus with tests on travis deploy

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,8 @@ echo "REMOTE_HOST = ${REMOTE_HOST}"
 
 echo -e "Host ssh.randy.tools\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-openssl aes-256-cbc -K $encrypted_af8de67677e8_key -iv $encrypted_af8de67677e8_iv -in deploy-front-travis.enc -out deploy-front-travis -d
+openssl aes-256-cbc -K $encrypted_af8de67677e8_key -iv $encrypted_af8de67677e8_iv -in deploy-front-travis.enc -out ~/.ssh/deploy-front-travis -d
+chmod 600 ~/.ssh/deply-front-travis
 
 eval "$(ssh-agent -s)"
 
