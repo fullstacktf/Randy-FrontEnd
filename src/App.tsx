d74eab6074fa2a3aa411eb5ae7acb5249cc0bad7
Ruben Zagon <ruben.zagon@gmail.com>
2019-12-18 15:16:11 +0000

    Change the state of userLogged

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const ContainterHome = styled.div`
 
 const App: React.FC = () => {
   //VOLVER VALOR A FALSE PARA TENER MODO LOGIN
-  const [isUserLogged, setIsUserLogged] = useState(true);
+  const [isUserLogged, setIsUserLogged] = useState(false);
 
   const handleLogin = () => {
     setIsUserLogged(true);
